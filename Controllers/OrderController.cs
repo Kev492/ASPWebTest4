ee0790f0c6c8d7d0dcb45e60af7b8107a6adeeaf
Kev492 <kyj12342000@gmail.com>
2023-11-25 21:06:53 +0900

    second commit

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -17,9 +17,26 @@ public class OrderController : Controller
         // Retrieve order information from the database using only ORDERLIST table
         var customerOrders = _context.ORDERLIST
             .Where(o => o.CustomerID == customerId)
+            .OrderByDescending(o => o.OrderDate)
             .ToList();
+        
+         // Create a list to store OrderDetails for each ORDERLIST item
+        var orderDetailsList = new List<List<OrderDetail>>();
 
+        // Retrieve order details from the database using ORDERDETAILS table
+        foreach (var order in customerOrders)
+        {
+            var details = _context.ORDERDETAILS
+                .Where(d => d.OrderID == order.OrderID)
+                .ToList();
+
+            orderDetailsList.Add(details);
+        }
+
+        // Pass both ORDERLIST and ORDERDETAILS lists to the view
+        ViewData["CustomerOrders"] = customerOrders;
+        ViewData["OrderDetailsList"] = orderDetailsList;
         // Pass the list of ORDERLIST models to the view
-        return View(customerOrders);
+        return View();
     }
 }
